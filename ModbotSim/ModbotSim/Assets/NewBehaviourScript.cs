75af38a65a354c89966e5510d8c54eca2fe21772
tania <tvt9@cornell.edu>
2015-10-22 12:18:21 -0400

    Adding Mini Modbot Simulation Code (in progress)

---
+++ ModbotSim/ModbotSim/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+		moveSpeed = 4f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey ("left"))
+		{
+			transform.Rotate(0f, -.75f , 0f);
+		}
+		if (Input.GetKey ("right")) 
+		{
+			transform.Rotate (0f, .75f, 0f);
+		}
+
+		transform.Translate (0f, 0f, Input.GetAxis ("Vertical")*moveSpeed*Time.deltaTime);
+	}
+}
