cfa9ff09c9ef15abf9a59304863eec41a6e9061d
Adit Arya <aryaadit@hotmail.com>
2015-11-14 05:25:18 -0500

    AI project

---
+++ ModbotPathPlan/Assets/Scripts/CarNavigator.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CarNavigator : MonoBehaviour
+{
+	public float accel = .005f;
+	public float setDeltaTurn = .1f;
+	private float currentDeltaTurn = 0f;
+	public float maxTurn = 2f;
+	public float maxSpeed = .045f;
+	public bool usingPhone = false;
+	private float speed = 0f;
+	private float turnValue = 0f;
+	private float turnInput = 0f;
+	private Vector3 direction = Vector3.forward;
+	private Vector3 rotationVector;
+	private bool goingBackwards = false;
+	private int forwardInput = 0;
+	
+	private string horizontal;
+	private string vertical;
+	
+	// Use this for initialization
+	void Start()
+	{
+		//var sw = new StreamWriter(Application.dataPath + "/Scripts/WriteToFile.txt", false);
+		//sw.Write(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t\n")); 
+		print("initializing " + this);
+		string[] name = this.ToString().Split(' ');
+		if (name[0] != "Kart")
+		{
+			throw new System.Exception("Movement script attached to non Kart object");
+		}
+		switch (name[1])
+		{
+		case "1":
+			horizontal = "Horizontal";
+			vertical = "Vertical";
+			break;
+		case "2":
+			horizontal = "Horizontal2";
+			vertical = "Vertical2";
+			break;
+		case "3":
+			horizontal = "Horizontal3";
+			vertical = "Vertical3";
+			break;
+		case "4":
+			horizontal = "Horizontal4";
+			vertical = "Vertical4";
+			break;
+		}
+	}
+	
+	
+	void Update()
+	{
+		//Vector3 Udp = Vector3.zero;
+		if (!usingPhone)
+			turnInput = Input.GetAxis(horizontal);
+		else
+		{
+			//Udp = GetComponent<UDPReceive>().getLatestUDPPacket();
+			//turnInput = Udp.x;
+			if (Mathf.Abs(turnInput) < .2)
+				turnInput = 0;
+		}
+		
+		//how quickly do we want to turn
+		currentDeltaTurn = setDeltaTurn * turnInput;
+		//takes care of ideling to go back straight && also turning
+		if (turnInput == 0 && turnValue > 0)
+		{
+			turnValue -= setDeltaTurn;
+		}
+		else if (turnInput == 0 && turnValue < 0)
+		{
+			turnValue += setDeltaTurn;
+		}
+		if ((turnValue + currentDeltaTurn) < (maxTurn * Mathf.Abs(turnInput)) && (turnValue + currentDeltaTurn) > (-maxTurn * Mathf.Abs(turnInput)))
+		{
+			turnValue += currentDeltaTurn;
+		}
+		//Debug.Log("DeltaTurn: " + currentDeltaTurn);
+		
+		//Debug.Log("Vert: "+Input.GetAxis(vertical));
+		//which way do we want to go
+		if (!usingPhone)
+		{
+			if (Input.GetAxis(vertical) > 0)
+				forwardInput = 1;
+			else if (Input.GetAxis(vertical) < 0)
+				forwardInput = -1;
+			else
+				forwardInput = 0;
+		}
+		else
+		{
+			/*float z = Udp.z;
+			if (z < -.25)
+				forwardInput = 1;
+			else if (z > 0)
+				forwardInput = -1;
+			else
+				forwardInput = 0;*/
+		}
+		
+		//takes care of acceleration
+		if (forwardInput == 1 && speed < maxSpeed)
+		{
+			speed += accel;
+		}
+		else if (forwardInput == -1 && speed > -maxSpeed)
+		{
+			speed -= accel;
+		}
+		else if (forwardInput == 0 && speed < 0)
+			speed += .5f * accel;
+		else if (forwardInput == 0 && speed > 0)
+			speed -= .5f * accel;
+		
+		//calculates the direction displacement vector
+		rotationVector.Set(0, turnValue * speed * 25, 0);
+		direction = Quaternion.Euler(rotationVector) * direction;
+		
+		//Debug.Log ("Direction: "+ direction);
+		//Debug.Log ("Speed: " +speed);
+	}
+	
+	//Update transform position and rotation - Write to file
+	void FixedUpdate()
+	{
+		//update the transform
+		transform.position += speed * direction;
+		if (!goingBackwards)
+			transform.rotation = Quaternion.LookRotation(direction);
+		else
+			transform.rotation = Quaternion.LookRotation(-1 * direction);
+		//WriteToRepo ();
+	}
+	
+	void WriteToRepo()
+	{
+		/* Vector3 pos = Vector3.zero;
+         Quaternion rot = transform.rotation;
+
+             pos = transform.position + speed * direction;
+             rot = Quaternion.LookRotation (direction);
+
+ */
+		int id = 1;
+		var sw = new StreamWriter(Application.dataPath + "/Scripts/WriteToFile.txt", true);
+		//print (transform.rotation.eulerAngles.y);
+		sw.Write(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t\n", id, transform.position.x, transform.position.y, transform.position.z, transform.rotation.eulerAngles.y));
+		sw.Close();
+		//Debug.Log("Wrote to files");     
+	}
+}
---
+++ ModbotPathPlan/Assets/Scripts/PathPlanning.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathPlanning : MonoBehaviour {
+
+	/* Set true for debug purposes */
+	public bool debug; 
+
+	/* Object representing the map */
+	public GenerateGraph map;
+
+	/* The path of the car represented as a list of nodes*/
+	public List<GenerateGraph.Node> path;
+
+	// Use this for initialization
+	public void Start () {
+		map = new GenerateGraph ();
+		print (map.ToString ());
+		//determinePath(map.startNode, map.endNode);
+	}
+
+	//draw nodes as spheres for debugging purposes
+	public void OnDrawGizmosSelected() {
+		foreach (Triangle triangle in map.meshTriangles) {
+			Gizmos.color = new Color(2f, 2f, 2f);
+			Gizmos.DrawSphere (triangle.Centroid(), 0.3f);
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine	(triangle.vertex1, triangle.vertex2);
+			Gizmos.DrawLine (triangle.vertex1, triangle.vertex3);
+			Gizmos.DrawLine (triangle.vertex2, triangle.vertex3);
+			Gizmos.DrawSphere (triangle.vertex1, 0.2f);
+			Gizmos.DrawSphere (triangle.vertex2, 0.2f);
+			Gizmos.DrawSphere (triangle.vertex3, 0.2f);
+		}
+	}
+
+	// <summary>
+	// Given two Node objects, determines a path between the startNode and the endNode
+	// </summary>
+	// <param name="startNode"> the start Node </param>
+	// <param name="endNode"> the end Node </param>
+	public void determinePath(GenerateGraph.Node startNode, GenerateGraph.Node endNode) {
+		if (startNode == null || endNode == null)
+			return;
+		PriorityQueue<GenerateGraph.Node> pq = new PriorityQueue<GenerateGraph.Node>(map.nodes.Count);
+		pq.queue(startNode);
+		Dictionary<GenerateGraph.Node, GenerateGraph.Node> came_from = new Dictionary<GenerateGraph.Node, GenerateGraph.Node>();
+		Dictionary<GenerateGraph.Node, float> cost_so_far = new Dictionary<GenerateGraph.Node, float> ();
+		came_from.Add(startNode, null);
+		cost_so_far.Add (startNode, 0);
+
+		while (pq.getSize() > 0) {
+			GenerateGraph.Node current = pq.dequeue();
+			if (current.Equals(endNode)) {
+				break;
+			}
+
+			for (int i = 0; i < current.neighbors.Count; i++) {
+				float new_cost = cost_so_far[current] + 
+					distanceBetweenNodes(current, current.neighbors[i]);
+				if (cost_so_far.ContainsKey(current.neighbors[i]) == false ||
+				    new_cost < cost_so_far[current.neighbors[i]]) {
+					cost_so_far[current.neighbors[i]] = new_cost;
+					current.neighbors[i].priority = new_cost;
+					pq.queue(current.neighbors[i]);
+					came_from[current.neighbors[i]] = current;
+				}
+			}
+		}
+
+		//Put nodes of the path into the list
+		path = new List<GenerateGraph.Node> ();
+		GenerateGraph.Node currentNode = endNode;
+		path.Add (currentNode);
+		while (currentNode.Equals(startNode) == false) {
+			currentNode = came_from[currentNode];
+			path.Add (currentNode);
+		}
+		path.Reverse();
+	}
+
+
+	// <summary>
+	// Given two Node objects, determines the distance between them. Specifically, the
+	// distance between the centroids of their triangles is returned.
+	// </summary>
+	// <param name="n1"> the first given Node </param>
+	// <param name="n2"> the second given Node </param>
+	public float distanceBetweenNodes(GenerateGraph.Node n1, GenerateGraph.Node n2) {
+		return Vector3.Distance(n1.triangle.Centroid (), n2.triangle.Centroid());
+	}
+ 
+
+
+}
