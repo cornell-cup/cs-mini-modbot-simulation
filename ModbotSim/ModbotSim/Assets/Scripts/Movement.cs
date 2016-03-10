e885a1749c91510227b64638faa79eea63560434
Kyler Ruvane <KylerRuvane@dhcp-rhodes-376.redrover.cornell.edu>
2016-03-10 02:13:01 -0500

    Added dynamic road generation

--- ModbotSim/ModbotSim/Assets/Scripts/Movement.cs
+++ ModbotSim/ModbotSim/Assets/Scripts/Movement.cs
@@ -33,10 +33,80 @@ public class Movement : MonoBehaviour
     private string vertical;
 	GetInput input;
 
+	//Log Stuff
+	private Vector3 position = new Vector3();
+	private Vector3 velocity = new Vector3();
+	private Vector3 acceleration = new Vector3();
+	private Vector3 rotation = new Vector3();
+	private Vector3 oldvelocity = new Vector3();
+
+
+	private Vector3 velocityX = new Vector3();
+	private Vector3 velocityY = new Vector3();
+	//constants
+
+	//force from motor
+//	maxTourque (scaling for forward input)
+//	Effeciency (assume 1)
+//	Gear Ratio (1 - 150 some int)
+//	Angular speed (rpms)
+//	Wheel Radius
+	private float mTor = 50;
+	private float effec = 1;
+	private float gearRatio = 20;
+	private float angularSpeed = 0;
+	private float wheelRad = .5f;
+
+	private float mass = 10;
+	private float g = 9.8f;
+	private float gradAngle = 0;
+
+	private float rollResist = 0;
+
+	private float airDensity = 1.21f;
+	private float dragCoef = 1.2f;
+	private float areaCross = .004f;
+
+	private float lW = .15f;
+
+	private float maxTurnAngle = Mathf.PI /6;
+
+	private Matrix4x4 posMatrix = new Matrix4x4 ();
+	private Matrix4x4 velMatrix = new Matrix4x4 ();
+	private Vector4 temp = new Vector4();
+
+	private Vector3 temp3 = new Vector3();
+
+
+//
+//	//
+//	uphill vs downhill (1 for uphill -1 for downhill)
+//	m =mass
+//	g = 9.8
+//	Gradient Angle (Gradient alpha) (angle of slope - 0)
+//	
+	//rolling resistance
+//	rolling resistance coeffecient (fr)
+//	wieight
+//	cos(alpha) slope
+//	
+	//drag force
+//	density of air (p=1.21 kg/m^3)
+//	drag coeffecient (cd=1.2)
+//	Area (cross sectional = .004 m^2)
+//	Velocity vector (y) squared magnitude
+//  Lw = car geoemetry = 0.15
+
+
+
+//mass m - ydot squared = velocity squared in forward direction
+	//tan(phi) phi is max turning angle (30 degrees) * turning input to get current phi
     // Use this for initialization
     void Start()
     {
 		input = GetComponent<GetInput>();
+		position.Set (50, 0, 50);
+		//position.y = 1.5f;
     }
 
 
@@ -44,7 +114,7 @@ public class Movement : MonoBehaviour
     {
 		turnInput = input.getTurnInput();
 		forwardInput = input.getForwardInput();
-
+//
         //how quickly do we want to turn
         currentDeltaTurn = DELTA_TURN * turnInput;
         //takes care of ideling to go back straight && also turning
@@ -103,16 +173,71 @@ public class Movement : MonoBehaviour
         //Debug.Log ("Speed: " +speed);
     }
 
+	void updateLogVectors(){
+		//oldvelocity = velocity;
+		//velocity = (position - transform.position) / Time.deltaTime;
+		//acceleration = (velocity - oldvelocity) / Time.deltaTime;
+		//position = transform.position;
+		//rotation = transform.rotation.eulerAngles;
+		//turn input [-1,1], forward input [-1,1]
+
+		float F1 = ((mTor * forwardInput) * effec + gearRatio * angularSpeed) / wheelRad;
+		Debug.Log ("F1: " + F1);
+		float F2 = mass * g * Mathf.Sin (gradAngle);
+		Debug.Log ("F2: " + F2);
+		float F3 = rollResist * mass * g * Mathf.Cos (gradAngle) ;
+		Debug.Log ("F3: " + F3);
+		float F4 = .5f * airDensity * dragCoef * areaCross * velocity.y;
+		Debug.Log ("F4: " + F4);
+
+		float accelY = (F1 - F2 - F3 - F4) / mass;
+
+		float accelX = (velocity.y * Mathf.Tan (turnInput * maxTurnAngle))/lW;
+
+//		temp.Set (transform.position.y, velocity.y, posY, 0);
+//		posMatrix.SetRow (0, temp);
+//
+//		temp.Set (transform.position.x, velocity.x, posX, 0);
+//		posMatrix.SetRow (1, temp);
+//
+//		temp.Set (0, 0, 0, 0);
+//		posMatrix.SetRow (2, temp);
+//
+//		temp.Set (0, 0, 0, 1);
+//		posMatrix.SetRow (3, temp);
+
+		//temp.Set (1, Time.deltaTime, .5f * Time.deltaTime * Time.deltaTime, 0);
+		//position = posMatrix * temp;
+		position.y = position.y + velocity.y *Time.deltaTime + accelY * Time.deltaTime * Time.deltaTime;
+		position.x = position.x + velocity.x * Time.deltaTime + accelX * Time.deltaTime * Time.deltaTime;
+
+		Debug.Log (velocity);
+
+		velocity.x = velocity.x + Time.deltaTime * accelX;
+		velocity.y = velocity.y + Time.deltaTime * accelY;
+
+
+		temp3.Set (position.x, position.z, position.y);
+		transform.position = temp3;
+		temp3.Set (velocity.x, 0, velocity.y);
+		//transform.rotation = Quaternion.LookRotation(temp3);
+
+
+
+
+	}
+
     //Update transform position and rotation - Write to file
     void FixedUpdate()
     {
-        //update the transform
-		transform.position += (speed * Time.deltaTime) * direction;
+       // update the transform
+			transform.position += direction* (speed * Time.deltaTime);
         if (!goingBackwards)
             transform.rotation = Quaternion.LookRotation(direction);
         else
             transform.rotation = Quaternion.LookRotation(-1 * direction);
-        //WriteToRepo ();
+		//updateLogVectors ();
+        WriteToRepo ();
     }
 
     void WriteToRepo()
@@ -127,7 +252,7 @@ public class Movement : MonoBehaviour
         int id = 1;
         var sw = new StreamWriter(Application.dataPath + "/Scripts/WriteToFile.txt", true);
         //print (transform.rotation.eulerAngles.y);
-        sw.Write(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t\n", id, transform.position.x, transform.position.y, transform.position.z, transform.rotation.eulerAngles.y));
+		sw.Write(string.Format("{0}\t{1}\t{2}\n",transform.position.x, transform.position.y, transform.position.z));
         sw.Close();
         //Debug.Log("Wrote to files");     
     }
