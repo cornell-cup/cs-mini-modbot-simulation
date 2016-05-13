3c385993940ae824d7bc46ac1de0527762ed463d
Danning Yao <danning.yao@gmail.com>
2016-05-13 13:00:22 -0400

    added placing and roxk assets

--- ModbotSim/Assets/Scripts/Place.cs
+++ ModbotSim/Assets/Scripts/Place.cs
@@ -2,17 +2,18 @@
 using System.Collections;
 
 public class Place : MonoBehaviour {
-    public int carPlace;
-    public int index;
-    public int numLaps;
+    public int carPlace=0;
+    public int index=0;
+    public int numNodes=0;
+    public int numLaps=0;
     public string[] arr = new string[2];
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         GameObject[] gos;
         gos = GameObject.FindGameObjectsWithTag("place");
         GameObject closest = null;
@@ -29,6 +30,16 @@ public class Place : MonoBehaviour {
             }
         }
         arr = closest.name.Split(' ');
-        index = int.Parse(arr[1]);
+        if (int.Parse(arr[1]) - index < 40 && index != int.Parse(arr[1])) { 
+            numNodes++;
+            index = int.Parse(arr[1]);
+        }
+            
+
+        if (numNodes > 40 && index > 80)
+        {
+            numLaps++;
+            numNodes = 0;
+        }
     }
 }
--- ModbotSim/Assets/Scripts/PlaceMaster.cs
+++ ModbotSim/Assets/Scripts/PlaceMaster.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 
 public class PlaceMaster : MonoBehaviour {
+    Rect position = new Rect(10, 10, 100, 20);
+    List<PlaceMapping> nameToIndex = new List<PlaceMapping>();
+    //Dictionary<GameObject, int> nameToIndex = new Dictionary<GameObject, int>();
+    GameObject[] karts;
+    GUIStyle myStyle = new GUIStyle();
+    private float time=0;
 
     private class PlaceMapping : System.IComparable<PlaceMapping>
     {
         public GameObject kart;
         public int index;
+        public string name;
 
         public PlaceMapping(GameObject o, int i)
         {
-
+            kart = o;
+            index = i;
         }
 
         public int CompareTo(PlaceMapping other)
@@ -24,26 +32,53 @@ public class PlaceMaster : MonoBehaviour {
             return other.index - index;
         }
     }
-    List<PlaceMapping> nameToIndex = new List<PlaceMapping>();
-    //Dictionary<GameObject, int> nameToIndex = new Dictionary<GameObject, int>();
-    GameObject[] karts;
+    
     // Use this for initialization
     void Start() {
+        Font f = new Font();
+        myStyle.normal.textColor = Color.white;
+        myStyle.fontSize = 24;
+        myStyle.fontStyle = FontStyle.Bold;
          karts = GameObject.FindGameObjectsWithTag("kart");
         foreach (GameObject o in karts){
             PlaceMapping k = new PlaceMapping(o, 0);
             nameToIndex.Add(k);
-        }       
-	}
-	
-	// Update is called once per frame
-	void Update () {
+            k.name = o.GetComponent<KartInfo>().name;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        time += Time.deltaTime;
+        if (time < 1)
+            return;
+        time = 0;
         for ( int i=0; i< nameToIndex.Count; i++)
         {
             PlaceMapping o = nameToIndex[i];
-            int ii = o.kart.GetComponent<Place>().index;
+            int ii = o.kart.GetComponent<Place>().index + 83* o.kart.GetComponent<Place>().numLaps;
             o.index = ii;
         }
         nameToIndex.Sort((x, y) => x.CompareTo(y));
+    }
+
+    void OnGUI()
+    {
+        for (int i = 0; i < nameToIndex.Count; i++)
+        {
+            position.y = 10 + 30 * i;
+            string x = "";
+            if (i == 0)
+                x = "1st";
+            if (i == 1)
+                x = "2nd";
+            if (i == 2)
+                x = "3rd";
+            if (i == 4)
+                x = "4th";
+            GUI.Label(position, nameToIndex[i].name+": "+x, myStyle);
+        }
+        
     }
 }
